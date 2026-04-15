eb8262233d8034828e607ca0b73185184894f18e
Daniel Fallman <dfallman@gmail.com>
2026-04-15 14:16:04 +0800

    removed unused scripts

--- src/bin/list_syntaxes.rs
+++
@@ -1,8 +0,0 @@
-use syntect_assets::assets::HighlightingAssets;
-fn main() {
-    let assets = HighlightingAssets::from_binary();
-    let ss = assets.get_syntax_set().unwrap();
-    for syntax in ss.syntaxes() {
-        println!("{} - {:?}", syntax.name, syntax.file_extensions);
-    }
-}
